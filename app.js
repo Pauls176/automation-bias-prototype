6136b5b5fdd0a97d385a7e11e5baf49bdd4a50e8
Pauls176 <pauline.von.petersdorff@studium.uni-hamburg.de>
2026-08-25 12:40:56 +0200

    Initialize Supabase client with URL and key

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+const SUPABASE_URL =
+    "https://gemtcvzzaaetckdivivu.supabase.co";
 
+const SUPABASE_PUBLISHABLE_KEY =
+    "sb_publishable_GLlEsjJQZhdM5csHPeQvVg_78L0jkxk";
+
+const supabaseClient =
+    window.supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_PUBLISHABLE_KEY
+    );
